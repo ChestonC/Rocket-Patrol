07e143707608eedfec13e6988dc5c26340f6053f
ChestonC <cchen208@ucsc.edu>
2021-04-09 15:48:51 -0700

    ship

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -14,12 +14,33 @@ class Play extends Phaser.Scene {
         this.starfield = this.add.tileSprite(
             0,0,640,480, 'starfield'
         ).setOrigin(0,0);
-        
+
         this.p1Rocket = new Rocket(
             this,
             game.config.width/2,
             game.config.height - borderUISize - borderPadding,
             'rocket'
+        );
+
+        this.ship1 = new Ship(
+            this,
+            100,
+            200,
+            'spaceship'
+        );
+
+        this.ship2 = new Ship(
+            this,
+            300,
+            240,
+            'spaceship'
+        );
+
+        this.ship3 = new Ship(
+            this,
+            380,
+            300,
+            'spaceship'
         );
 
         // green UI background
@@ -30,22 +51,25 @@ class Play extends Phaser.Scene {
             borderUISize * 2,
             0x00FF00,
             ).setOrigin(0,0);
-            
-        // define keys
-        keyF = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F);
-        keyR = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.R);
-        keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
-        keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
+
         // white borders
 	    this.add.rectangle(0, 0, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0 ,0);
 	    this.add.rectangle(0, game.config.height - borderUISize, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0 ,0);
 	    this.add.rectangle(0, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0 ,0);
 	    this.add.rectangle(game.config.width - borderUISize, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0 ,0);
+
+        keyF = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F);
+        keyR = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.R);
+        keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
+        keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);      
     }
 
     update() {
-        this.starfield.tilePositionX -= 1;
+        this.starfield.tilePositionX -= 4;
         this.p1Rocket.update();
-    }
 
+        this.ship1.update();
+        this.ship2.update();
+        this.ship3.update();
+    }
 }
