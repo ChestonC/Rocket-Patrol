65cb8bb8fe918810bc13c3c0e65c77631aaf77db
ChestonC <cchen208@ucsc.edu>
2021-04-10 14:32:08 -0700

    a

--- src/Main.js
+++ src/Main.js
@@ -2,7 +2,7 @@ let config = {
     type: Phaser.CANVAS,
     width: 640,
     height: 480,
-    scene: [Menu, Play],
+    scene: [Menu, Play]
 }
 
 let game = new Phaser.Game(config);
@@ -11,4 +11,4 @@ let borderUISize = game.config.height / 15;
 let borderPadding = borderUISize / 3;
 
 // reserve keyboard vars
-let keyF, keyR, keyLEFT, keyRIGHT;
+//let keyF, keyR, keyLEFT, keyRIGHT;
