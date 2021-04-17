8ffa28039ce58935481f6f97baff8496f4928f7c
ChestonC <cchen208@ucsc.edu>
2021-04-17 14:58:46 -0700

    Update Main.js

--- src/Main.js
+++ src/Main.js
@@ -1,5 +1,6 @@
 // Add your own (copyright-free) background music to the Play scene (5) Credit: www.bensound.com
 // Allow the player to control the Rocket after it's fired (5)
+// Create a new scrolling tile sprite for the background (5)
 // Redesign the game's artwork, UI, and sound to change its theme/aesthetic (to something other than sci-fi) (60)
 // Sound effects by: www.fesliyanstudios.com
 
